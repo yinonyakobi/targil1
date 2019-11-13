5d10f7e628db294ea0b791ddf0730cff2147c70d
yinon <yinon@LAPTOP-SKEFSOKM>
2019-11-13 18:51:26 +0200

    part1

--- part1/Program.cs
+++ part1/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace part1
 {
+
     class Program
     {
+        public const int ArraySize = 20;
+        public const int MinRand = 18;
+        public const int MaxRand = 122;
+        static Random r = new Random(DateTime.Now.Millisecond);
         static void Main(string[] args)
         {
+            int[] A = new int[ArraySize];
+            int[] B = new int[ArraySize];
+            int[] C = new int[ArraySize];
+
+            for (int i = 0; i < ArraySize; ++i)
+            {
+                A[i] = r.Next(MinRand, MaxRand);
+                B[i] = r.Next(MinRand, MaxRand);
+
+                C[i] = A[i] - B[i];
+                if (C[i] < 0) C[i] = -C[i];
+            }
+
+            printArray(A);
+            printArray(B);
+            printArray(C);
+        }
+
+        private static void printArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write("{0, -4}", i);
+            }
+            Console.WriteLine();
         }
     }
 }
