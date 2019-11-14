3c4d02ada4be262f63d35c1c04f0f376c2fd2282
yinon <yinon@LAPTOP-SKEFSOKM>
2019-11-14 12:42:44 +0200

    consts added

--- part2/Program.cs
+++ part2/Program.cs
@@ -8,28 +8,30 @@ namespace part2
 {
     class Program
     {
+        public const int NumOfMonthsInAYear = 12;
+        public const int NumOfDaysInAMonth = 31;
         static void Main(string[] args)
         {
-            bool[,] occupied = new bool[12, 31];
-            occupied.Initialize(false);
+            bool[,] occupied = new bool[NumOfMonthsInAYear, NumOfDaysInAMonth];
+            occupied.Initialize();
 
 
             bool exit = false;
-            int decision;
-            while (!exit) 
+            char decision;
+            while (!exit)
             {
                 printMenu();
-                
-                decision = Console.Read();
+                decision = (char)Console.Read();
                 switch (decision)
                 {
-                    case 1:
+                    case '1':
                         break;
-                    case 2:
+                    case '2':
                         break;
-                    case 3:
+                    case '3':
+                        exit = false;
                         break;
-                    case 4:
+                    case '4':
                         exit = true;
                         break;
                 }
@@ -42,14 +44,27 @@ namespace part2
         static void printMenu()
         {
             Console.WriteLine(
-            @"
-            To book a date enter: 1
+            @"To book a date enter: 1
 
-            To show occupied dates enter: 2
+To show occupied dates enter: 2
 
-            To show the number of booked days and percentage of days that are booked enter: 3
+To show the number of booked days and percentage of days that are booked enter: 3
 
-            To exit enter: 4");
+To exit enter: 4
+");
+        }
+        static void countAndPrintOccupiedNumberAndPercentage(bool[,] calender)
+        {
+            int counter = 0;
+            for (int i = 0; i < NumOfMonthsInAYear; i++)
+            {
+                for (int j = 0; j < NumOfDaysInAMonth; j++)
+                {
+                    if (calender[i, j])
+                        ++counter;
+                }
+            }
         }
     }
 }
+
